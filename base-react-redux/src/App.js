98882e6dadcdbe43deec46a5c1c4b27bb6fb1788
LaDuy <laduy2208@gmail.com>
2024-05-30 16:39:48 +0700

    docker and react bootstrap

--- base-react-redux/src/App.js
+++ base-react-redux/src/App.js
@@ -1,9 +1,4 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
 // class App extends React.Component {
 //   render() {
@@ -35,14 +30,7 @@ import React from "react";
 // };
 
 const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    <div className="App-container">
-      <MyComponent></MyComponent>
-    </div>
-  );
+  return <div className="app-container">hello world</div>;
 };
 
 export default App;

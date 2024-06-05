248d3741b7f2c84873e483c9bccbca0737538cf6
LaDuy <laduy2208@gmail.com>
2024-06-05 16:15:19 +0700

    register component

--- base-react-redux/src/components/Auth/Login.js
+++ base-react-redux/src/components/Auth/Login.js
@@ -10,8 +10,26 @@ const Login = (props) => {
 
   const navigate = useNavigate();
 
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
   const handleLogin = async () => {
     //validate
+    const isValidEmail = validateEmail(email);
+    if (!isValidEmail) {
+      toast.error("Invalid Email");
+      return;
+    }
+
+    if (!password) {
+      toast.error("Invalid password");
+      return;
+    }
     //submitAPI
     let data = await postLogin(email, password);
 
@@ -29,7 +47,7 @@ const Login = (props) => {
     <div className="login-container">
       <div className="header">
         <span>Don't have an account yet?</span>
-        <button>Sign up</button>
+        <button onClick={() => navigate("/register")}>Sign up</button>
       </div>
       <div className="title col-4 mx-auto">React Learning</div>
       <div className="welcome col-4 mx-auto">Hello, who's this?</div>

1c6833652c328ea589e949e695a25a7f1b97b67c
LaDuy <laduy2208@gmail.com>
2024-06-07 16:12:42 +0700

    video 88

--- base-react-redux/src/components/Users/DetailQuiz.js
+++ base-react-redux/src/components/Users/DetailQuiz.js
@@ -81,6 +81,32 @@ const DetailQuiz = (props) => {
     }
   };
 
+  const handleFinishQuiz = () => {
+    let payload = {
+      quizId: +quizId,
+      answers: [],
+    };
+    let answers = [];
+    if (dataQuiz && dataQuiz.length > 0) {
+      dataQuiz.forEach((question) => {
+        let questionId = question.questionId;
+        let userAnswerId = [];
+
+        question.answers.forEach((a) => {
+          if (a.isSelected === true) {
+            userAnswerId.push(a.id);
+          }
+        });
+        answers.push({
+          questionId: +questionId,
+          userAnswerId: userAnswerId,
+        });
+      });
+
+      payload.answers = answers;
+    }
+  };
+
   return (
     <div className="detail-quiz-container">
       <div className="left-content">
@@ -105,7 +131,10 @@ const DetailQuiz = (props) => {
           <button className="btn btn-primary" onClick={() => handleNext()}>
             Next
           </button>
-          <button className="btn btn-primary" onClick={() => handleNext()}>
+          <button
+            className="btn btn-warning"
+            onClick={() => handleFinishQuiz()}
+          >
             Finish
           </button>
         </div>
